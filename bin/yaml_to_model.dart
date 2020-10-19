dcdfb8f5a5fbf6635ef4fe9c9ee46ed2079354a3
Issam Alameh <ialameh@actionablecoaching.com>
2020-10-19 17:23:08 +1100

    v 0.0.1 to test

---
+++ bin/yaml_to_model.dart
@@ -0,0 +1,38 @@
+import 'package:yaml/yaml.dart';
+import "package:path/path.dart" show  join, normalize;
+import 'dart:convert';
+import 'dart:io';
+
+main(List<String> arguments) async {
+  if (arguments.length != 1) {
+    exit(999);
+  }
+  // String yaml = arguments[0];
+  String yaml = 'models.dart';
+  final currentDirectory = Directory.current.path.toString();
+  print(currentDirectory);
+  final filePath = normalize(join(currentDirectory, yaml));
+  String yamlRawData = '';
+  try {
+    yamlRawData = new File(filePath).readAsStringSync();
+  } catch (e) {
+    print('you need to have properly formated models.yaml file');
+    print('please refer to documentation');
+    exit(0);
+
+  }
+  var doc = loadYaml(yamlRawData);
+  var jsonString = json.encode(doc);
+  print(jsonString);
+  // Map<String, dynamic> model = jsonDecode(jsonString);
+  // Map<String, dynamic> input = jsonDecode(jsonString);
+  //
+  // model[node][0].removeWhere((key, value) => key == "meta");
+  // model[node][0].forEach((key, value) =>
+  //     model[node][0][key]
+  //         .removeWhere((key, value) => key.toString().endsWith('_structure')));
+  // model[node][0].forEach((key, value) {
+  //   model[node][0].update(key, (value) => '');
+  // });
+
+}
---
+++ lib/yaml_to_model.dart
@@ -0,0 +1,8 @@
+library yaml_to_model;
+
+/// A Model Generator.
+class ReadYaml {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
+
